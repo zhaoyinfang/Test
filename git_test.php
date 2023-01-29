b269db4e142fef6c72ec1516441d6f45d016633f
zhaoyinfang <foreverfzy2008@hotmail.com>
2023-01-29 15:08:13 +0100

    V2V2

--- git_test.php
+++ git_test.php
@@ -46,11 +46,11 @@ form {
 <form id="registerform" action="for_test1.php" method="post">
     <p>which vehicle do you have right now?</p>
     <input type="checkbox" id="vehicle1" name="vehicle[]" value="Bike">
-    <label for="vehicle1"> I have a bike</label>
+    <label for="vehicle1"> I have a bike!</label>
     <input type="checkbox" id="vehicle2" name="vehicle[]" value="Car">
-    <label for="vehicle2"> I have a car</label>
+    <label for="vehicle2"> I have a car!</label>
     <input type="checkbox" id="vehicle3" name="vehicle[]" value="Boat">
-    <label for="vehicle3"> I have a motorcycle</label><br>
+    <label for="vehicle3"> I have a Boat!</label><br>
 <input type="submit" value="SUBMIT" class="submit">
 </form>
 
